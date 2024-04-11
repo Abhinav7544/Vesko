0ec80981ad515a7f07a34b27de05446b5799db98
Abhinav7544 <abhinav99sinha@gmail.com>
2024-04-11 18:45:54 +0530

    test

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,13 +8,6 @@ const Navbar = () => {
       {/* Sidebar */}
       <Sidebar/>
       <div className="wrapper">
-        <motion.span
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          Lama Dev
-        </motion.span>
         <div className="social">
           <a href="#">
             <img src="/facebook.png" alt="" />
